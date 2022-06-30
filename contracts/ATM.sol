c8d4d709db5921d88976daa193cfe22876618eb1
davydovMikhail <davydov27977@gmail.com>
2022-06-30 18:48:05 +0700

    init

---
+++ contracts/ATM.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract ATM {
+    using SafeMath for uint256;
+
+    uint256 public price;
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, 
+        "Only available to the owner");
+        _;
+    }
+
+    function setNewPrice(uint256 _price) external onlyOwner {
+        price = _price;
+    }
+
+    // function getPrice(uint256 amount) internal view returns(uint256) {
+        
+    // }
+
+    // function buyCoin() public {
+
+    // }
+}
